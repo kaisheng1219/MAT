b2f2cd8870e4c3abc5fbc982659506a567592093
Ng Kai Sheng <1201302264@student.mmu.edu.my>
2021-03-28 19:27:32 +0800

    test

---
+++ Adventure Game/main.py
@@ -0,0 +1,9 @@
+def intro():
+    print("--------------------------")
+    print("  Mission Against Terror  ")
+    print("--------------------------")
+    print(" 1) Single Player")
+    print(" 2) Multiplayer") 
+
+def 
+intro()
